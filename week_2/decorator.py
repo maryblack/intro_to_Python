7a7faf4778edb8f432dd29098834f7733d0e1c80
maryblack <maniya.chernova@gmail.com>
2019-07-31 17:28:03 +0300

    week_2 on course

---
+++ week_2/decorator.py
@@ -0,0 +1,22 @@
+import functools
+
+
+def logger(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        result = func(*args, **kwargs)
+        with open('log.txt', 'w') as f:
+            f.write(str(result))
+
+        return result
+    return wrapped
+
+
+@logger
+def summator(num_list):
+    return sum(num_list)
+
+
+print('Summator: {}'.format(summator([1, 2, 3, 4])))
+
+print(summator.__name__)
---
+++ week_2/num_to_str.py
@@ -0,0 +1,4 @@
+input_list = [1, 4, 5, 6, 99, 100]
+print (type(input_list[0]))
+new_list = list(map(str, input_list))
+print(type(new_list[0]), new_list)
